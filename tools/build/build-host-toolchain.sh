598acc3435dfe6956f9394522cca5d9b424dede0
Yuta Saito <kateinoigakukun@gmail.com>
2023-12-12 06:47:13 +0000

    Revert "ci: disable sccache on arm64 macOS for now"
    This reverts commit 420b2be9c3d20e3a9c31acbc90c064402e1e256d.

--- tools/build/build-host-toolchain.sh
+++ tools/build/build-host-toolchain.sh
@@ -21,22 +21,10 @@ case $(uname -s) in
 esac
 
 build_host_toolchain() {
-  # WORKAROUND: It seems that sccache v0.7.4 mis-reuses compilation results during cmark build
-  # and causes a build failure. We need to disable sccache on arm64 macOS for now.
-  local use_sccache="${USE_SCCACHE:-1}"
-  if [ "$(uname -s)" == "Darwin" ] && [ "$(uname -m)" == "arm64" ]; then
-    use_sccache=0
-  fi
-
-  local sccache_flags=""
-  if [ "$use_sccache" -eq 1 ]; then
-    sccache_flags="--sccache"
-  fi
   # Build the host toolchain and SDK first.
   "$SOURCE_PATH/swift/utils/build-script" \
     --preset-file="$TOOLS_BUILD_PATH/build-presets.ini" \
     --preset=$HOST_BUILD_PRESET \
-    $sccache_flags \
     HOST_ARCHITECTURE="$(uname -m)" \
     INSTALL_DESTDIR="$HOST_TOOLCHAIN_DESTDIR"
 }
