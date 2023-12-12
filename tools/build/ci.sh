7c0445439faf23d4884c9809e3c18f8e5fd8fe57
Yuta Saito <kateinoigakukun@gmail.com>
2023-12-12 06:46:14 +0000

    ci: Use older sccache on Apple Silicon

--- tools/build/ci.sh
+++ tools/build/ci.sh
@@ -9,5 +9,30 @@ SCHEME="${1:?"scheme is not specified"}"
 export SCCACHE_CACHE_SIZE="50G"
 export SCCACHE_DIR="$SOURCE_PATH/build-cache"
 
+SCCACHE_INSTALL_PATH="$SOURCE_PATH/build-sdk/sccache"
+
+install_sccache_if_needed() {
+  local version="v0.6.0"
+  local url="https://github.com/mozilla/sccache/releases/download/${version}/sccache-${version}-aarch64-apple-darwin.tar.gz"
+
+  if [ -e "$SCCACHE_INSTALL_PATH" ]; then
+    return
+  fi
+
+  # WORKAROUND: It seems that sccache v0.7.4 mis-reuses compilation results during cmark build
+  # only on Apple Silicon. So we use older version for now.
+  if [ "$(uname)" != "Darwin" ] || [ "$(uname -m)" != "arm64" ]; then
+    return
+  fi
+
+  mkdir -p "$SCCACHE_INSTALL_PATH"
+
+  (cd "$SCCACHE_INSTALL_PATH" && curl -L "$url" | tar xz --strip-components=1)
+
+}
+
+install_sccache_if_needed
+export PATH="$SCCACHE_INSTALL_PATH:$PATH"
+
 $TOOLS_BUILD_PATH/build-toolchain.sh "$SCHEME"
 $TOOLS_BUILD_PATH/package-toolchain --scheme "$SCHEME" --daily-snapshot
