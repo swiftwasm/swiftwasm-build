672fad8c945782dfe7c901baca56edb52aee2e02
Yuta Saito <kateinoigakukun@gmail.com>
2024-01-28 15:31:43 +0000

    Trigger all builds when swift-sdk-generator changes

--- .github/scripts/build-matrix.rb
+++ .github/scripts/build-matrix.rb
@@ -93,6 +93,7 @@ def affected_schemes(changes, schemes)
     prefixes = [
       "schemes/#{scheme}/",
       "tools/build/",
+      "tools/swift-sdk-generator",
       "tools/git-swift-workspace",
     ]
     changes.any? do |change|
