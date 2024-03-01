9e21cb6edb98048da0e2d45a64d3644e209c680a
Yuta Saito <kateinoigakukun@gmail.com>
2024-03-01 07:30:56 +0000

    Add validation check for Foundation.Bundle

---
+++ test/toolchain/foundation/Bundle.swift
@@ -0,0 +1,14 @@
+// RUN: rm -rf %t.dir
+// RUN: mkdir -p %t.dir
+// RUN: %{swiftc} -target wasm32-wasi -o %t.dir/check.wasm %s -resource-dir %{package_path}/usr/lib/swift_static -sdk %{package_path}/usr/share/wasi-sysroot
+// RUN: %{wasm_run} --dir %t.dir::/tmp --dir %t.dir::/tmp2 %t.dir/check.wasm | FileCheck %s
+
+import Foundation
+
+// Bundle.main is derived from the path of the executable and cwd
+// CHECK: bundlePath: /tmp
+chdir("/tmp")
+print("bundlePath:", Bundle.main.bundlePath)
+
+// CHECK: Bundle(path:).bundlePath: /tmp2
+print("Bundle(path:).bundlePath:", Bundle(path: "/tmp2")?.bundlePath ?? "nil")
