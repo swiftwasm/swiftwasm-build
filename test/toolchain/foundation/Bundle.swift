1c3bf32d48b71a3665af521b0b12ae7267569efb
Yuta Saito <kateinoigakukun@gmail.com>
2024-03-01 16:19:57 +0000

    Fix FileCheck reference and always install wasmtime

--- test/toolchain/foundation/Bundle.swift
+++ test/toolchain/foundation/Bundle.swift
@@ -1,7 +1,7 @@
 // RUN: rm -rf %t.dir
 // RUN: mkdir -p %t.dir
 // RUN: %{swiftc} -target wasm32-wasi -o %t.dir/check.wasm %s -resource-dir %{package_path}/usr/lib/swift_static -sdk %{package_path}/usr/share/wasi-sysroot
-// RUN: %{wasm_run} --dir %t.dir::/tmp --dir %t.dir::/tmp2 %t.dir/check.wasm | FileCheck %s
+// RUN: %{wasm_run} --dir %t.dir::/tmp --dir %t.dir::/tmp2 %t.dir/check.wasm | %{FileCheck} %s
 
 import Foundation
 
--- test/toolchain/foundation/FileManager.swift
+++ test/toolchain/foundation/FileManager.swift
@@ -1,7 +1,7 @@
 // RUN: %{swiftc} -target wasm32-wasi -o %t.wasm %s -resource-dir %{package_path}/usr/lib/swift_static -sdk %{package_path}/usr/share/wasi-sysroot
 // RUN: rm -rf %t.dir
 // RUN: mkdir -p %t.dir
-// RUN: %{wasm_run} --dir %t.dir::/tmp --dir %S/Inputs::/Inputs %t.wasm | FileCheck %s
+// RUN: %{wasm_run} --dir %t.dir::/tmp --dir %S/Inputs::/Inputs %t.wasm | %{FileCheck} %s
 
 import Foundation
 
