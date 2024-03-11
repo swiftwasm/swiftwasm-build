ad29b3e9193e2c600735b386215fe8dd88516873
Yuta Saito <kateinoigakukun@gmail.com>
2024-03-11 13:04:31 +0000

    Update XCTest patch

--- tools/build/build-xctest.sh
+++ tools/build/build-xctest.sh
@@ -17,15 +17,16 @@ cd $BUILD_DIR
 cmake -G Ninja \
   -DCMAKE_BUILD_TYPE="Release" \
   -DCMAKE_SYSROOT="$WASI_SYSROOT_PATH" \
+  -DCMAKE_STAGING_PREFIX="$DESTINATION_TOOLCHAIN/usr" \
   -DCMAKE_Swift_COMPILER="$SWIFT_BIN_DIR/swiftc" \
   -DCMAKE_TOOLCHAIN_FILE="$TOOLS_BUILD_PATH/toolchain-wasi.cmake" \
   -DLLVM_BIN="$LLVM_BIN_DIR" \
   -DCLANG_BIN="$CLANG_BIN_DIR" \
   -DBUILD_SHARED_LIBS=OFF \
   -DCMAKE_Swift_COMPILER_FORCED=ON \
-  -DCMAKE_Swift_FLAGS="-resource-dir $DESTINATION_TOOLCHAIN/usr/lib/swift_static" \
+  -DCMAKE_Swift_FLAGS="-sdk $WASI_SYSROOT_PATH -resource-dir $DESTINATION_TOOLCHAIN/usr/lib/swift_static" \
   -DSWIFT_FOUNDATION_PATH=$DESTINATION_TOOLCHAIN/usr/lib/swift_static/wasi/wasm32 \
   "${SOURCE_PATH}/swift-corelibs-xctest"
   
 ninja -v
-DESTDIR="$DESTINATION_TOOLCHAIN/usr/lib" ninja -v install
+ninja -v install
