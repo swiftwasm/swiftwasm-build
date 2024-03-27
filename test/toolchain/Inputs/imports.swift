c6225ce8610a2012cc251819387133bd1a5a8424
Yuta Saito <kateinoigakukun@gmail.com>
2024-03-27 02:29:15 +0000

    Include `libxml2` in the final toolchain
    We just forgot to include it for long time.
    
    Close https://github.com/swiftwasm/swift/issues/5575

--- test/toolchain/Inputs/imports.swift
+++ test/toolchain/Inputs/imports.swift
@@ -2,11 +2,10 @@ import Foundation
 import XCTest
 import WASILibc
 
+#if canImport(FoundationXML)
+  import FoundationXML
+#endif
 // FIXME: This should be supported on swiftwasm branch
-// #if canImport(FoundationXML)
-//   #error("FoundationXML should not be able to import now")
-// #endif
-// 
 // #if canImport(FoundationNetworking)
 //   #error("FoundationNetworking should not be able to import now")
 // #endif
