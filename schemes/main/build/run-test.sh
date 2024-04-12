0be4bc4a536f4b14eede9b8c8873aa1ab606fb20
Yuta Saito <kateinoigakukun@gmail.com>
2024-04-12 06:31:49 +0000

    Filter out some tests using sil-opt

--- schemes/main/build/run-test.sh
+++ schemes/main/build/run-test.sh
@@ -5,4 +5,5 @@ set -euxo pipefail
 SOURCE_PATH="$(cd "$(dirname "$0")/../../../.." && pwd)"
 TARGET_BUILD_ROOT="$SOURCE_PATH/build/WebAssembly"
 
-ninja check-swift-wasi-wasm32-custom -C "$TARGET_BUILD_ROOT/swift-stdlib-wasi-wasm32"
+env "LIT_FILTER_OUT=(IRGen/|embedded/)"
+ninja check-swift-wasi-wasm32-custom -C "$TARGET_BUILD_ROOT/wasmstdlib-linux-x86_64"
