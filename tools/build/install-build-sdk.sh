8a5bac8f7315defba1c830b066605215645657e8
Yuta Saito <kateinoigakukun@gmail.com>
2024-05-25 03:09:36 +0000

    Install libxml2 and ICU with threads support

--- tools/build/install-build-sdk.sh
+++ tools/build/install-build-sdk.sh
@@ -10,22 +10,25 @@ SCHEME_DIR="$(cd "$(dirname "$0")/../../schemes/$SCHEME" && pwd)"
 CURRENT_SCHEME_FILE="$BUILD_SDK_PATH/scheme"
 
 install_libxml2() {
-  LIBXML2_URL="https://github.com/swiftwasm/libxml2-wasm/releases/download/1.0.0/libxml2-wasm32-unknown-wasi.tar.gz"
-  curl -L "$LIBXML2_URL" | tar xz
-  rm -rf "$BUILD_SDK_PATH/libxml2"
-  mv libxml2-wasm32-unknown-wasi "$BUILD_SDK_PATH/libxml2"
+  read -r -a LIBXML2_URLS <<< "$(python3 -c 'import sys, json; print(" ".join(json.load(sys.stdin)["libxml2"]))' < "$SCHEME_DIR/manifest.json")"
+  for url in "${LIBXML2_URLS[@]}"; do
+    curl -L "$url" | tar xz -C "$BUILD_SDK_PATH"
+  done
+  # For backward compatibility
+  if [ "$SCHEME" = "release-5.9" ] || [ "$SCHEME" = "release-5.10" ] || [ "$SCHEME" = "release-6.0" ]; then
+    ln -sf libxml2-wasm32-unknown-wasi "$BUILD_SDK_PATH/libxml2"
+  fi
 }
 
 install_icu() {
-  local ICU_URL
-  ICU_URL="$(python3 -c 'import sys, json; print(json.load(sys.stdin)["icu4c"])' < "$SCHEME_DIR/manifest.json")"
-  curl -L "$ICU_URL" | tar Jx
+  read -r -a ICU_URLS <<< "$(python3 -c 'import sys, json; print(" ".join(json.load(sys.stdin)["icu4c"]))' < "$SCHEME_DIR/manifest.json")"
   rm -rf "$BUILD_SDK_PATH/icu"
-  if [ -d "icu_out" ]; then
-    # Just for backward compatibility
-    mv icu_out "$BUILD_SDK_PATH/icu"
-  else
-    mv icu "$BUILD_SDK_PATH/icu"
+  for url in "${ICU_URLS[@]}"; do
+    curl -L "$url" | tar Jx -C "$BUILD_SDK_PATH"
+  done
+  # Just for backward compatibility
+  if [ -d "$BUILD_SDK_PATH/icu_out" ]; then
+    mv $BUILD_SDK_PATH/icu_out "$BUILD_SDK_PATH/icu"
   fi
 }
 
