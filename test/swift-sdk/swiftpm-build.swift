c2ded01671f2ec2ada5b663a667d2ab7ba067715
Yuta Saito <kateinoigakukun@gmail.com>
2024-10-27 17:21:11 +0000

    XFAIL: FoundationXML with SwiftPM build

--- test/swift-sdk/swiftpm-build.swift
+++ test/swift-sdk/swiftpm-build.swift
@@ -3,3 +3,4 @@
 // RUN: %{swift} package init --package-path %t.dir --name Example
 // RUN: cp %S/Inputs/imports.swift %t.dir/Sources/Example/Imports.swift
 // RUN: %{target_swift_build} --package-path %t.dir
+// XFAIL: scheme=main
