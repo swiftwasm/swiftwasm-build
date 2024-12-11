8c2305077054014f90d2de1dd815b2dedb2844b3
Yuta Saito <kateinoigakukun@gmail.com>
2024-12-11 16:06:19 +0000

    Use the new driver for testing for now
    should be reverted once https://github.com/swiftlang/swift-driver/pull/1756 is merged

--- schemes/main/build/run-test.sh
+++ schemes/main/build/run-test.sh
@@ -6,4 +6,5 @@ SOURCE_PATH="$(cd "$(dirname "$0")/../../../.." && pwd)"
 TARGET_BUILD_ROOT="$SOURCE_PATH/build/WebAssembly"
 
 HOST_SUFFIX=$(find "$TARGET_BUILD_ROOT" -name "wasmstdlib-*" -exec basename {} \; | sed 's/wasmstdlib-//')
-env "PATH=$TARGET_BUILD_ROOT/llvm-$HOST_SUFFIX/bin:$PATH" "LIT_FILTER_OUT=(IRGen/|embedded/)" ninja check-swift-wasi-wasm32-custom -C "$TARGET_BUILD_ROOT/wasmstdlib-$HOST_SUFFIX"
+# Force using the new driver for testing until https://github.com/swiftlang/swift-driver/pull/1756
+env SWIFT_FORCE_TEST_NEW_DRIVER=1 "PATH=$TARGET_BUILD_ROOT/llvm-$HOST_SUFFIX/bin:$PATH" "LIT_FILTER_OUT=(IRGen/|embedded/)" ninja check-swift-wasi-wasm32-custom -C "$TARGET_BUILD_ROOT/wasmstdlib-$HOST_SUFFIX"
