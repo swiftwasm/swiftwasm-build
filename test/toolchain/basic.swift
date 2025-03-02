be4c7238a4d1675b1650dd5518d60367dab253fc
Yuta Saito <kateinoigakukun@gmail.com>
2025-03-02 08:28:02 +0000

    test: Remove toolchain tests

--- test/toolchain/basic.swift
+++
@@ -1,3 +0,0 @@
-// RUN: %{swiftc} -target wasm32-unknown-wasi -static-stdlib -sdk %{package_path}/usr/share/wasi-sysroot %s -o %t.wasm
-// RUN: %{wasm_run} %t.wasm
-print("Hello")
--- test/toolchain/clang-module.swift
+++
@@ -1,6 +0,0 @@
-// RUN: rm -rf %t.dir
-// RUN: mkdir -p %t.dir
-// RUN: %{swift} build --package-path %S/Inputs/clang-module-example --scratch-path %t.dir --triple wasm32-unknown-wasi --static-swift-stdlib
-
-// Skipping this test on main until we include swift-testing in the SDK
-// REQUIRES: GH-5587
