56de29b5bbf436b76078be0711cd8fe6f21d805f
Yuta Saito <kateinoigakukun@gmail.com>
2025-03-02 08:42:15 +0000

    Drop 5.9 and 5.10 support from build system

--- schemes/release-5.10/build/run-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-SOURCE_PATH="$(cd "$(dirname "$0")/../../../.." && pwd)"
-TARGET_BUILD_ROOT="$SOURCE_PATH/build/WebAssembly"
-
-ninja check-swift-wasi-wasm32-custom -C "$TARGET_BUILD_ROOT/swift-stdlib-wasi-wasm32"
